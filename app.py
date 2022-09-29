f1ca025e014275de230b606f5d79391a821bbb12
Zachary Stout <stout.zachary@gmail.com>
2022-09-29 16:14:55 -0600

    added postgres

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 # from flask_cors import CORS
-import os
+# import os
 
 app = Flask(__name__)
 # CORS(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://gvonrcurugclxl:e0372921977ec611807c8fd971e1ebbab11b497afe14c1e09021702094ca02e8@ec2-54-91-223-99.compute-1.amazonaws.com:5432/df7snlolbiee27"
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
